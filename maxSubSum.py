d27b440939b120d4219dfc20feb311cadd03acbf
wuchongyao <wuchongyao@outlook.com>
2019-06-23 13:27:26 +0800

    "first modified"

--- maxSubSum.py
+++ maxSubSum.py
@@ -3,9 +3,10 @@
 # date:2019-06-23
 
 def maxSubSum(array):
+    thisSum = 0
     length = len(array)
     maxSum = 0
-    for(i=0;i<length;i++):
+    for i in range(length):
         thisSum += array[i]
         if thisSum < 0:
             thisSum = 0
@@ -17,7 +18,7 @@ def maxSubSum(array):
 
 def main():
     array = [1,-1,3,-4,7,8,-2,9]
-    maxSubsum(array)
+    print(maxSubSum(array))
 
 
 if __name__ == "__main__":
